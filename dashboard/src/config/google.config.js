da61efcf973af767fd137638a42a0ef3cb02afa0
cardozoarthur <arthurgcardozo@gmail.com>
2021-05-30 21:56:33 -0300

    Remove dashboard dependency

--- dashboard/src/config/google.config.js
+++
@@ -1,6 +0,0 @@
-
-const config = {
-    api_key: 'AIzaSyCR1JOUMIfsr0jbxUV9s9oo7X0aehkUVio'
-}
-
-export default config; 
--- dashboard/src/styles.js
+++
@@ -1,8 +0,0 @@
-import styled, {createGlobalStyle} from 'styled-components';
-
-export const Global = createGlobalStyle`
-    body {
-        padding: 0;
-        margin: 0;
-    }
-`;
